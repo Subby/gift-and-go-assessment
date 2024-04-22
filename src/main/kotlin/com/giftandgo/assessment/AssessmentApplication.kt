3d1a0c5e564055b12fb957a7309f279f2ad6b28e
Denver Fernandes <sub367@gmail.com>
2024-04-22 12:05:40 +0100

    Remove debug listener

--- src/main/kotlin/com/giftandgo/assessment/AssessmentApplication.kt
+++ src/main/kotlin/com/giftandgo/assessment/AssessmentApplication.kt
@@ -2,11 +2,6 @@ package com.giftandgo.assessment
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.ApplicationContext
-import org.springframework.context.ApplicationListener
-import org.springframework.context.event.ContextRefreshedEvent
-import org.springframework.stereotype.Component
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping
 
 
 @SpringBootApplication
@@ -14,13 +9,4 @@ class AssessmentApplication
 
 fun main(args: Array<String>) {
 	runApplication<AssessmentApplication>(*args)
-}
-
-@Component
-class EndpointsListener : ApplicationListener<ContextRefreshedEvent> {
-	override fun onApplicationEvent(event: ContextRefreshedEvent) {
-		val applicationContext: ApplicationContext = event.applicationContext
-		applicationContext.getBean(RequestMappingHandlerMapping::class.java).handlerMethods
-			.forEach { println(it) }
-	}
 }
